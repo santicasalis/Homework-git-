9d55a35329aacffc3eab66923851f1b017c3860f
santicasalis <santicasalis@hotmail.com>
2023-11-20 12:08:46 -0300

    santi pureba 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
   if (numero % 2 === 0) {
-    return numero.toString(2);
+    return numero.toString(8);
   } else {
-    return numero.toString(6);
+    return numero.toString(7);
   }
 }
